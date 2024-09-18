268061ed8810ff8438dd08b47b4acb0c911cd4e2
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-09-18 14:37:24 +0200

    Vérification des méthodes de Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -23,16 +23,23 @@ public class Gaulois {
 	}
 	
 	public void frapper(Romain romain) {
-		System.out.println(nom + "envoie un grand coup dans la mâchoire de " + romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
 
 	@Override
 	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+		return "Gaulois [nom=" + nom + ", force=" + force + "]";
 	}
 	
 	public static void main(String[] args) {
-		// TODO test de la classe
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		
+		System.out.println(asterix.prendreParole()); // Test prendreParole
+		asterix.parler("Bonjour, je m'appelle " + asterix.getNom() + "." ); // Test parler
+		
+		Romain minus = new Romain("Minus", 3);
+		asterix.frapper(minus); // Test frapper
+		asterix.frapper(minus);
 	}
 }
