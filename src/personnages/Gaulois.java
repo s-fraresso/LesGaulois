b394b0145c3cbf0e0e763c43c196f0b794e56162
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-09-18 14:55:36 +0200

    Le gaulois boit la potion

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -24,7 +24,12 @@ public class Gaulois {
 	
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup(effetPotion * force / 3);
+	}
+	
+	public void boirePotion(int forcePotion) {
+		effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée.");
 	}
 
 	@Override
@@ -34,13 +39,9 @@ public class Gaulois {
 	
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
-		asterix.parler("Bonjour à tous");
-		
 		Romain minus = new Romain("Minus", 6);
-		minus.parler("UN GAU... UN GAUGAU...");
 		
-		asterix.frapper(minus);
-		asterix.frapper(minus);
+		asterix.boirePotion(3);
 		asterix.frapper(minus);
 	}
 }
