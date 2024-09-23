47de79d81fa6ea0152704905ec9c018bf05fdc52
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-09-23 18:40:31 +0200

    Invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert this.force >= 0;
 	}
 	
 	public String getNom() {
@@ -22,11 +23,20 @@ public class Romain {
 	}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert force >= 0;
+		int forcePrev = force;
+		
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aïe !");
 		} else {
 			parler("J'abandonne...");
 		}
+		
+		assert forcePrev - force < 0;
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
 	}
 }
