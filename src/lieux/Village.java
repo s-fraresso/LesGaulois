e1624e4484b6b6e65ee86f3d3dbe008a589d03c6
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-09-23 18:08:43 +0200

    Ajout attribut chef dans Village

--- src/lieux/Village.java
+++ src/lieux/Village.java
@@ -1,10 +1,17 @@
 package lieux;
 
+import personnages.Chef;
+
 public class Village {
 	private String nom;
+	private Chef chef;
 
 	public Village(String nom) {
 		this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 	
 	public String getNom() {
