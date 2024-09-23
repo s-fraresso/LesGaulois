480bcd2eedcc75960717ed987e462aa4459d0d9c
Sylvain Fraresso <frs4938a@ensfsi.onmicrosoft.com>
2024-09-23 18:18:00 +0200

    Ajout de ajouterHabitant et trouverHabitant dans Village

--- src/lieux/Village.java
+++ src/lieux/Village.java
@@ -1,13 +1,37 @@
 package lieux;
 
 import personnages.Chef;
+import personnages.Gaulois;
 
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois villageois[];
+	private int nbVillageois = 0;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public boolean ajouterHabitant(Gaulois gaulois) {
+		if (nbVillageois < villageois.length) {
+			villageois[nbVillageois++] = gaulois;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public Gaulois trouverHabitant(int numVillageois) {
+		if (numVillageois < nbVillageois) {
+			return villageois[numVillageois];
+		}
+		else {
+			System.out.println("Le villageois recherché n'existe pas.");
+			return void;
+		}
 	}
 	
 	public void setChef(Chef chef) {
